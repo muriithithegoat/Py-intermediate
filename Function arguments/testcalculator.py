b4a908073d5099cb30e6705edb9d159856463c8f
muriithithegoat <kinyuaangelo@gmail.com>
2025-06-11 18:56:23 +0300

    calculator

---
+++ Function arguments/testcalculator.py
@@ -0,0 +1,6 @@
+#this is a calculator program that takes two numbers and an operator as input and returns the result of the operation
+#by use of functions of course
+def add(x,y):
+  return x + y
+
+add(7,8)
