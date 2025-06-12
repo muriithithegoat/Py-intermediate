e2b8c8b1d29b158eee8c26984e7add8de5ae3a78
muriithithegoat <kinyuaangelo@gmail.com>
2025-06-12 11:42:52 +0300

    final code

--- Function arguments/argsKwargs.py
+++ Function arguments/argsKwargs.py
@@ -1,7 +1,13 @@
 #learning about *args and **kwargs
-def order_pizza(size, *toppings):
-  print(f"Ordered a {size} pizza.")
-  print("Toppings:")
+def order_pizza(size, *toppings, **details):
+  print(f"Ordered a {size} pizza with the following toppings:")
+  for topping in toppings:
+    print(f"- {topping}")
+  for key, value in details.items():
+    print(f"- {key}: {value}")
+# This function takes a size, a variable number of toppings, and additional details like delivery and tip.
+
+# Example usage of the function 
 
 
-order_pizza("large", "pepperoni", "mushrooms")
+order_pizza("large", "pepperoni", "mushrooms", delivery=True, tip=5)
